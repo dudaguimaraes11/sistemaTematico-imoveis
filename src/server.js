d1bf9631d0d5a2a8d7b55cc27bec834b419676c3
FelipeCampos_ <felipe.a.campos7@aluno.senai.br>
2026-03-30 11:05:05 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,22 +1,21 @@
 import express from 'express';
 import 'dotenv/config';
+import path from 'path'; 
+import { fileURLToPath } from 'url'; 
 import imoveisRoutes from './routes/imoveisRoute.js';
 import clientesRoutes from './routes/clientesRoute.js';
 
 const app = express();
 app.use(express.json());
 
-const PORT = process.env.PORT || 3001;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-docApiSwagger(app)({
-    info: {
-        title: 'API de exemplo - Documentação Swagger',
-        version: '1.0.0',
-        description: 'Esta é a documentação da API de exemplo usando Swagger.',
-    },
-    baseDir: import.meta.dirname,
-    filesPattern: './**/*.js',
-});
+
+
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+const PORT = process.env.PORT || 3001;
 
 app.get('/', (req, res) => {
     res.send('🚀 API funcionando');
@@ -25,6 +24,17 @@ app.get('/', (req, res) => {
 // Rotas
 app.use('/api', imoveisRoutes);
 app.use('/api', clientesRoutes);
+
+// Tratamento de Erros do Multer
+app.use((err, req, res, next) => {
+    if (err.code === 'LIMIT_FILE_SIZE') {
+        return res.status(400).json({ error: 'Arquivo muito grande. Máximo 5MB.' });
+    }
+    if (err.message === 'Formato de imagem inválido. Use JPEG ou PNG.') {
+        return res.status(400).json({ error: err.message });
+    }
+    next(err);
+});
 
 app.use((req, res) => {
     res.status(404).json({ error: 'Rota não encontrada' });
