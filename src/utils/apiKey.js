6581bf79b94422c691d36b6aec93449154949939
Melfreitass <melissa.g.freitas@aluno.senai.br>
2026-04-06 16:25:22 -0300

    feat: arruma erro apiKey e server.js

--- src/utils/apiKey.js
+++ src/utils/apiKey.js
@@ -1,7 +1,7 @@
 import 'dotenv/config'
 
 const autenticar = (req, res, next) => {
-    const chave = req.headers['X-API-Key'];
+    const chave = req.headers['x-api-key'];
 
     if (!chave || chave !== process.env.API_KEY) {
         return res.status(401).json({ error: 'Acesso não autorizado. Chave de API inválida'})
