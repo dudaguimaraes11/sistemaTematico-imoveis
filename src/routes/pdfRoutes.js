257e27f87c6229898a47e9cdcae5649b8ca0e097
Maria Eduarda Ferreira <maria.e.guimaraes19@aluno.senai.br>
2026-04-09 21:20:06 -0300

    Feat: Adiciona pdfRoutes

--- src/routes/pdfRoutes.js
+++ src/routes/pdfRoutes.js
@@ -1,20 +1,10 @@
 import express from 'express';
-import upload from '../utils/uploadConfig.js';
 import * as controller from '../controllers/pdfController.js';
 
 const router = express.Router();
 
-/**
- * ROTA DE UPLOAD
- * O 'upload.single('foto')' extrai o arquivo enviado pelo Postman/Frontend
- * e o coloca dentro de 'req.file' para o controller usar.
- */
-router.post('/:id/foto', upload.single('foto'), controller.uploadFoto);
+router.get('/pdf', controller.gerarPdfTodos);
 
-/**
- * ROTA DE VISUALIZAÇÃO
- * Retorna o link ou os dados da foto do imóvel
- */
-router.get('/:id/foto', controller.verFoto);
+router.get('/:id/pdf', controller.gerarPdfImovel);
 
 export default router;
