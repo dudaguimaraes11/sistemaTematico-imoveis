624b5b8aef2ecd72d9dc7547f972b5fd7859e995
felipe antunes campos <felipe.a.campos7@aluno.senai.br>
2026-04-09 21:52:03 -0300

    Feat: Repara uploads fotos

--- src/routes/pdfRoutes.js
+++ src/routes/pdfRoutes.js
@@ -1,10 +1,9 @@
 import express from 'express';
-import * as controller from '../controllers/pdfController.js';
+import * as controller from '../controllers/PdfController.js';
 
 const router = express.Router();
 
-router.get('/pdf', controller.gerarPdfTodos);
-
-router.get('/:id/pdf', controller.gerarPdfImovel);
+router.get('/todos', controller.imoveisTodos); 
+router.get('/:id', controller.imovelPorId);
 
 export default router;
