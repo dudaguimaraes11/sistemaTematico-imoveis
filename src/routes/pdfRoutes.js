79c970280075b35af060398de046fbc310d09e66
felipe antunes campos <felipe.a.campos7@aluno.senai.br>
2026-04-09 21:50:36 -0300

    Feat: Repara pdfRoutes

--- src/routes/pdfRoutes.js
+++ src/routes/pdfRoutes.js
@@ -1,12 +1,4 @@
 import express from 'express';
-<<<<<<< HEAD
-import * as controller from '../controllers/PdfController.js'; // Ajuste o nome do arquivo se necessário
-
-const router = express.Router();
-
-router.get('/pdf', controller.imoveisTodos);
-router.get('/:id', controller.imovelPorId);
-=======
 import * as controller from '../controllers/pdfController.js';
 
 const router = express.Router();
@@ -16,7 +8,3 @@ router.get('/pdf', controller.gerarPdfTodos);
 router.get('/:id/pdf', controller.gerarPdfImovel);
 
 export default router;
-
-/* Arruma erros */ 
->>>>>>> 20acfbd029995db8e632dbe77e5c8ac2a8b611da
-
